747a9807f1a68ea73abdb4ad30a752f20620d9c5
kartikbanga <71484653+kartikbanga@users.noreply.github.com>
2023-09-26 00:04:38 +0530

    Update Rotate Image.cpp

--- Arrays/Rotate Image.cpp
+++ Arrays/Rotate Image.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
+        // Transpose the matrix
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)
                 swap(matrix[i][j],matrix[j][i]);
         }
+        
+        // reverse each row,because the resultant matrix should be rotated 90deg clockwise.
         for(int i=0;i<n;i++)
             reverse(matrix[i].begin(),matrix[i].end());
         
